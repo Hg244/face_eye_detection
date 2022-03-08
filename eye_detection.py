765df07c79fc1e9bddbc1fcc7744590b0e97a169
HirutG <88872303+Hg244@users.noreply.github.com>
2022-03-08 10:27:37 +0300

    phase 1

---
+++ eye_detection.py
@@ -0,0 +1,23 @@
+import numpy as np 
+import cv2
+cap=cv2.VideoCapture(0)
+face=cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+eye=cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
+while True:
+     ret,frame=cap.read()
+     gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+     faces=face.detectMultiScale(gray,1.3,5)
+     for(x,y,w,h) in faces:
+         cv2.rectangle(frame,(x,y),(x+w,y+h),(255,200,0),5)
+         roi_gray=gray[y:y+w,x:x+w]
+         roi_color=frame[y:y+h,x:x+w] 
+         eyes=eye.detectMultiScale(roi_gray,1.3,5) 
+         for (ex,ey,ew,eh)in eyes:
+             cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(189,255,0),5)          
+     cv2.imshow('frame',frame)
+
+    
+     if cv2.waitKey(1)==ord('c'):
+         break
+         cap.release()
+         cv2.destroyAllWindows()
